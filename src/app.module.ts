a0d2f80991de834bc4e57fa22782d138386fe876
Sarad Singh <sarad.singh@introcept.co>
2021-10-11 09:53:14 +0545

    integrate database
        - configure prisma ORM with sqlite for database
        - create prisma schema for users and posts
        - create database tables with prisma migrate
        - use prisma client in NestJs service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './db/prisma.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PrismaService],
   controllers: [AppController],
   providers: [AppService],
 })
